b1e8186ead46e8de118a0672a914a9eabcb7aeb1
ishaanko <imishaanko@gmail.com>
2024-12-10 20:52:29 -0800

    fixed small issues added zeropowerbehavior to Sample.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/TwoWheelPinpointIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/localizers/TwoWheelPinpointIMU.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.common.hardware.drivetrain;
+package org.firstinspires.ftc.teamcode.pedroPathing.localization.localizers;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
