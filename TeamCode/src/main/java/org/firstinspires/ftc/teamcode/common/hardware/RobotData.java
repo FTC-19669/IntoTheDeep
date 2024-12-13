a94b9f543f4ba971b2b62120b69a456c5295f820
ishaanko <imishaanko@gmail.com>
2024-12-12 18:28:34 -0800

    started robot data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/RobotData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/RobotData.java
@@ -1,4 +1,47 @@
 package org.firstinspires.ftc.teamcode.common.hardware;
 
 public class RobotData {
+    public long loopTime = System.currentTimeMillis();
+
+    public boolean scoring = false;
+    public boolean sampleLoaded = false;
+    public boolean specimenLoaded = false;
+    public boolean intakingSample = false;
+    public boolean intakingSpecimen = false;
+
+    public void startScoring() {
+        scoring = true;
+        intakingSample = false;
+    }
+
+    public void stopScoring() {
+        scoring = false;
+        intakingSample = false;
+    }
+
+    public void startIntaking() {
+        scoring = false;
+        intakingSample = true;
+    }
+
+    public void stopIntaking() {
+        scoring = false;
+        intakingSample = false;
+    }
+
+    public void setSampleLoaded() {
+        sampleLoaded = true;
+    }
+
+    public void setSampleUnloaded() {
+        sampleLoaded = false;
+    }
+
+    public void setSpecimenLoaded() {
+        specimenLoaded = true;
+    }
+
+    public void setSpecimenUnloaded() {
+        specimenLoaded = false;
+    }
 }
